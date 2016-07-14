9079d1a2487c915b9d3cf8ded3249db664ead632
Matthew <matbulat@gmail.com>
2016-07-14 16:31:13 +0100

    Added Summation

--- src/versioncontrollab/VersionControlLab.java
+++ src/versioncontrollab/VersionControlLab.java
@@ -8,11 +8,14 @@ public class VersionControlLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        summation();
     }
     
     public static void summation(){
-        
+        int a=2;
+        int b=3;
+        int c = a+b;
+        System.out.println(c);
     }
         
     public static void multiplication(){
