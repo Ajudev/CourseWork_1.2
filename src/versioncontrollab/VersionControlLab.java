7aabd4cff8576d7fb6ffc9a64e50b1fc18fea59c
Matthew <matbulat@gmail.com>
2016-07-14 16:25:07 +0100

    init Commit

---
+++ src/versioncontrollab/VersionControlLab.java
@@ -0,0 +1,30 @@
+
+package versioncontrollab;
+
+
+public class VersionControlLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void summation(){
+        
+    }
+        
+    public static void multiplication(){
+        
+    }
+    
+    public static void subtraction(){
+        
+    }
+    
+    public static void exponentiation(){
+        
+    }
+    
+}
