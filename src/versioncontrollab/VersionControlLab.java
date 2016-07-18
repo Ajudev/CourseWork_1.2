83e37b50b8cd002106599902011d77f4ee724fe8
Mateusz1 <m.bulat@mdx.ac.uk>
2016-07-18 13:49:50 +0100

    Update VersionControlLab.java

--- src/versioncontrollab/VersionControlLab.java
+++ src/versioncontrollab/VersionControlLab.java
@@ -8,25 +8,23 @@ public class VersionControlLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        summation();
+        summation(2,5);
     }
     
-    public static void summation(){
-        int a=2;
-        int b=3;
+    public static void summation(int a,int b){
         int c = a+b;
         System.out.println(c);
     }
         
-    public static void multiplication(){
+    public static void multiplication(int a, int b){
         
     }
     
-    public static void subtraction(){
+    public static void subtraction(int a, int b){
         
     }
     
-    public static void exponentiation(){
+    public static void exponentiation(int a, int b){
         
     }
     
