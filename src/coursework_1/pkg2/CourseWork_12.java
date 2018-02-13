9a3ec91cb349900457acdd7f5e747588e7e29fda
Ajudev <ajudevnair@gmail.com>
2018-02-13 12:57:42 +0400

    no synchronization and written all final code along with the file storing code.

--- src/coursework_1/pkg2/CourseWork_12.java
+++ src/coursework_1/pkg2/CourseWork_12.java
@@ -39,45 +39,46 @@ public class CourseWork_12 {
             int choice=s.nextInt();
             if (choice==1){
                 long accountno=9876543210L;
-                System.out.println("The Bank account has been created - "+accountno+" with account balance of $1980");
+                System.out.println("The Bank account with "+accountno+" and an account balance of $1980 has been created");
             }
             if(choice==2){
                 while (true){
-                    System.out.println("Enter your name - ");
+                    System.out.println("Please enter your name:");
                     String name=s.next();
-                    System.out.println("Enter your surname - ");
+                    System.out.println("Please enter your surname: ");
                     String surname=s.next();
                     ArrayList<Double> transaction_List=new ArrayList<>();                                                                  //temp Arraylist to be filled by user
                     while (true){
-                        System.out.println("Enter the amount depicted by - or + before the amount to withdraw or deposit");
+                        System.out.println("Please enter the transactions you wish to make with a - before the amount for withdrawing the amount");
                         double amount=s.nextDouble();
-                        System.out.println("Do you wish to continue adding transactions? Enter y for yes and n for no");
+                        System.out.println("Do you wish to write more transactions type yes or no: ");
                         String c=s.next();
                         if(c.equals("yes")){
-                            System.out.println("You have selected to do more transactions");
+                            System.out.println("You have chosen to continue writing transactions");
                             transaction_List.add(amount);
                         }
                         if (c.equals("no")){
+                            System.out.println("Transactions recorded");
                             break;    
                         }
                         else{
-                            System.out.println("You have entered an incorrect option and are exiting the program");
+                            System.out.println("Please choose the right option");
                             break;
                         }   
                     }
                     USER.add(new user(name,surname,bankacc,transaction_List));                                                            
                     i++;                                                                                                    
-                    System.out.println("Do you wish to continue adding users? Enter y for yes and n for no");
+                    System.out.println("Do you wish to add more users type yes or no: ");
                     String c1=s.next(); 
                 
                     if (c1.equals("yes")) {
-                        System.out.println("You have selected to add more users");
+                        System.out.println("You have chosen to add more users");
                     }
                     if (c1.equals("no")) {
-                        System.out.println("You have selected to stop adding users. Please run the simulation after this process");
+                        System.out.println("Please run the simulation");
                         break;
                     } else {
-                        System.out.println("You have selected an incorrect/invalid option so the program is assuming you want to stop entering users");
+                        System.out.println("You have typed an invalid command");
                         break;
                     }
                 }
